183c03b0565f0b3b7549f2a3046b182d490fcfef
Simon Brugel <sbrugel62@gmail.com>
2023-04-29 14:40:08 -0400

    feat: login and register pages (no styling) - login endpoint added to server - router component set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 import {Navingbar} from './Navbar';
 import { UserPage } from './UserPage';
+import Login from './components/Login';
+import Register from './components/Register';
+
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom'
 function App() {
+  const [user, setLoginUser] = useState({});
+
   return (
     <div className="App">
-      <UserPage></UserPage>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={user && user._id ? <UserPage/> : <Login setLoginUser={ setLoginUser } />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/login" element={<Login setLoginUser={ setLoginUser } />} />
+        </Routes>
+    </div>
     </div>
   );
 }
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
+
+const Login = ({ setLoginUser }) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState({
+    name: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  const login = () => {
+    const { name, password } = user;
+    if (!name || !password) {
+      toast.error("Missing a field!");
+      return;
+    }
+    axios.post("http://localhost:5000/login", user).then((res) => {
+      toast(res.data.message);
+      setLoginUser(res.data.user);
+      navigate("/");
+    });
+  };
+
+  return (
+    <>
+      <div>
+        <h1>Login to your account</h1>
+        <p>
+          Don't have an account? <a href="/register">Register here.</a>
+        </p>
+        <div>
+          <form action="#">
+            <div>
+              <input
+                type="text"
+                id="sign-in-name"
+                name="name"
+                value={user.name}
+                onChange={handleChange}
+                placeholder="Username"
+                autocomplete="off"
+              />
+            </div>
+            <div class="flex flex-col mb-6">
+              <input
+                type="password"
+                id="sign-in-password"
+                name="password"
+                value={user.password}
+                onChange={handleChange}
+                placeholder="Password"
+              />
+            </div>
+            <div class="flex w-full">
+              <button
+                type="submit"
+                onClick={(e) => {
+                  e.preventDefault();
+                  login();
+                }}
+              >
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <ToastContainer />
+    </>
+  );
+};
+
+export default Login;
