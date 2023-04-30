3a69f9f128d7e5942d05e1bb97af05a234e79e95
craftablescience <me@craftablescience.info>
2023-04-29 22:08:38 -0400

    feat: add card backgrounds to login

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -40,7 +40,7 @@ const Login = ({ setLoginUser }) => {
   return (
     <>
 	  <Navingbar userID={0} />
-      <div className="go-login-wrapper">
+      <div className="go-login-wrapper go-card">
 	    <h1>Login to your account</h1>
         <p>
           Don't have an account? <a href="/register">Register here.</a>
