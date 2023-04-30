7b134be751a91b063facd05b4a5e4eda4c4597ce
Simon Brugel <sbrugel62@gmail.com>
2023-04-30 08:57:38 -0400

    feat: People Page progress bars

--- client/src/PeoplePage.jsx
+++ client/src/PeoplePage.jsx
@@ -4,6 +4,8 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { Card } from "react-bootstrap";
 import {Image }from "react-bootstrap";
+import ProgressBar from 'react-bootstrap/ProgressBar';
+
 const PeoplePage = ({ currentUser }) => {
     const navigate = useNavigate();
 
@@ -20,22 +22,39 @@ const PeoplePage = ({ currentUser }) => {
 
     useEffect(() => {
         if (!users) return;
+
+        let numLocations;
+        axios.get("http://localhost:5000/locations")
+            .then(async (res) => {
+                numLocations = res.data.length;
+                const userPromises = users.map(async (u) => {
+                    let visited = 0;
+                    await axios.get("http://localhost:5000/events/byuser/" + u.id)
+                      .then((res) => {
+                        visited = res.data.length;
+                      });
+                    return (
+                      <div onClick={() => navigate("/user/" + u.id)}>
+                        <Card className="profile-Card">
+                          <div style={{ padding: "20px", backgroundColor: "var(--BG-COLOR-SECONDARY)" }}>
+                            <Image thumbnail src="https://www.kindpng.com/picc/m/24-248253_user-profile-default-image-png-clipart-png-download.png" />
+                            <Card.Body className="profile-card-description">
+                              <a href="#">{u.name}</a>
+                              <p>{visited} of {numLocations} visited.</p>
+                              <ProgressBar min={0} now={visited} max={numLocations} />
+                            </Card.Body>
+                          </div>
+                        </Card>
+                      </div>
+                    );
+                  });
+                
+                  const resolvedUsers = await Promise.all(userPromises);
+                  setUsersList(resolvedUsers);
+                
+            })
         
-        const ulUsers = users.map((u) => {
-            return <div onClick={() => navigate("/user/" + u.id)}>
-                <Card className="profile-Card">
-               
-                    <div style={{padding:"20px", backgroundColor:"var(--BG-COLOR-SECONDARY)"}}>
-                <Image thumbnail src="https://www.kindpng.com/picc/m/24-248253_user-profile-default-image-png-clipart-png-download.png"  />
-                    <Card.Body className="profile-card-description">
-                    
-                <a>{ u.name }</a>
-                    </Card.Body>
-                    </div>
-                </Card>
-            </div>
-        })
-        setUsersList(ulUsers);
+        
     }, [users])
 
     return (
